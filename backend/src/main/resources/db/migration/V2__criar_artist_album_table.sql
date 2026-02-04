a02121cec2985c5c155e41a29b6ab8317ecffe87
andremartins30 <andre.martins30@hotmail.com>
2026-02-04 18:28:02 +0000

    feat(database): altera tipos de dados para BIGSERIAL e BIGINT nas tabelas artists e albums

--- backend/src/main/resources/db/migration/V2__criar_artist_album_table.sql
+++ backend/src/main/resources/db/migration/V2__criar_artist_album_table.sql
@@ -1,14 +1,15 @@
 CREATE TABLE artists (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE albums (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(200) NOT NULL,
-    artist_id INTEGER NOT NULL,
+    artist_id BIGINT NOT NULL,
     cover_object_name VARCHAR(255),
     CONSTRAINT fk_album_artist
         FOREIGN KEY(artist_id)
         REFERENCES artists (id)
+);
 );
